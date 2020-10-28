e0fae96631ba26d9c653f94be29fe9a4a5c87d5d
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2020-10-28 23:24:42 +0100

    Improve code generator. Now code can contain numbers 0-9 and lowercase Latin characters a-z. The new maximum length for the code is 36.

--- src/bullscows/Main.java
+++ src/bullscows/Main.java
@@ -7,14 +7,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter the secret code's length:");
         int codeLength = scanner.nextInt();
+        System.out.println("Input the number of possible symbols in the code:");
+        int numberOfSymbols = scanner.nextInt();
         scanner.nextLine();
-        while (codeLength > 10) {
-            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
-            System.out.println("Please enter a number not greater than 10:");
+        while (codeLength > 36) {
+            System.out.println("Error: can't generate a secret number with a length of " + codeLength + " because there aren't enough unique digits.");
+            System.out.println("Please enter a number not greater than 36:");
             codeLength = scanner.nextInt();
             scanner.nextLine();
         }
-        String secretCode = generateRandomCode(codeLength);
+        String secretCode = generateRandomCode(codeLength, numberOfSymbols);
+        System.out.print("The secret code is prepared: ");
+        for (int i = 0; i < codeLength; i++) {
+            System.out.print('*');
+        }
+        if (numberOfSymbols < 11) {
+            System.out.print(" (0 - " + (numberOfSymbols - 1) + ")\n");
+        } else {
+            System.out.print(" (0 - 9) (" + (char) 97 + " - " + (char) (97 + numberOfSymbols - 11) + ")\n");
+        }
         int i = 1;
         System.out.println("Okay, let's start a game!");
         System.out.println("Turn " + i + ":");
@@ -52,13 +63,15 @@ public class Main {
         }
     }
 
-    static String generateRandomCode(int length) {
+    static String generateRandomCode(int length, int numberOfSymbols) {
+        String chars = "0123456789abcdefghijklmnopqrstuvwxyz";
         Random random = new Random();
         StringBuilder code = new StringBuilder();
         while (code.length() != length) {
-            String num = String.valueOf(random.nextInt(10));
-            if (!code.toString().contains(num)) {
-                code.append(num);
+            int index = random.nextInt(numberOfSymbols);
+            char digit = chars.charAt(index);
+            if (!code.toString().contains(String.valueOf(digit))) {
+                code.append(digit);
             }
         }
         return code.toString();
